65f8dfa8d175b9303e0e533373a366bfdaf8f594
arif <hossaininfo@ymail.com>
2022-01-28 14:05:33 +0100

    created StudentController and Student class

---
+++ src/main/java/com/example/spring_security_2022/student/StudentController.java
@@ -0,0 +1,27 @@
+package com.example.spring_security_2022.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+    private final List<Student> STUDENTS = Arrays.asList(
+            new Student(1, "Arif Java"),
+            new Student(2, "Jacob Html"),
+            new Student(3, "Robin Css")
+    );
+
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId){
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student with id "+studentId+" does not exits"));
+    }
+}
